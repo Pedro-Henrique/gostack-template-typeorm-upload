66818ccb468fbadc0db897fe93c15de8e74b8e27
Pedro <cspedroh@gmail.com>
2020-08-31 19:46:59 -0300

    Resoluçao do desafio

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(transactionId: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne({
+      where: {
+        id: transactionId,
+      },
+    });
+
+    if (!transaction) {
+      throw new AppError('Transaction not found');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
